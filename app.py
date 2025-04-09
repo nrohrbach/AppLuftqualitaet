3a5b88d1dd72efed7d4d474c81157391f2729460
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2025-04-09 06:58:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,15 +36,21 @@ def create_map(center):
         control_scale=True,
         tiles="https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
         attr='Map data: &copy; <a href="https://www.swisstopo.ch" target="_blank" rel="noopener noreferrer">swisstopo</a>, <a href="https://www.housing-stat.ch/" target="_blank" rel="noopener noreferrer">BFS</a>',
-        )
-        # Zweite WMTS-Ebene hinzufügen
-        folium.TileLayer(
+    )
+    
+    # Zweite WMTS-Ebene hinzufügen
+    folium.TileLayer(
         tiles="https://wmts.geo.admin.ch/1.0.0/ch.bafu.luftreinhaltung-schwefeldioxid/default/current/3857/{z}/{x}/{y}.jpeg",
         attr='Map data: &copy; <a href="https://www.swisstopo.ch" target="_blank" rel="noopener noreferrer">swisstopo</a>',
-        name='Swiss TLM3D',
+        name='Luftreinhaltung Schwefeldioxid',
         overlay=True
-        ).add_to(m)
+    ).add_to(m)
+    
+    # Layer Control hinzufügen, um zwischen den Ebenen zu wechseln
+    folium.LayerControl().add_to(m)
+    
     return m
+
 
 
 # App
