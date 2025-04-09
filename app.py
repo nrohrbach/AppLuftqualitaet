e81225c076a5079e8df77c33a31689b7fee1772a
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2025-04-09 07:28:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,18 +77,34 @@ if gemeinde:
     # Create a pandas DataFrame from the data
     df = pd.DataFrame(data, columns=['Year', 'RasterValue'])
     fig = px.bar(df, x='Year', y='RasterValue', title='Schwefeldioxid in µg/m³')
+
     # Hinzufügen einer Linie bei Grenzwert von 30
-    fig.add_shape(
-        type="line",
-        x0=df['Year'].min(), x1=df['Year'].max(),
-        y0=30, y1=30,
-        line=dict(color="Red", width=2, dash="dash"),
+fig.add_shape(
+    type="line",
+    x0=df['Year'].min(), x1=df['Year'].max(),
+    y0=30, y1=30,
+    line=dict(color="Red", width=2, dash="dash"),
+    name='Grenzwert 30 µg/m³'
+)
+
+# Aktualisieren des Layouts, um die Legende hinzuzufügen und weitere Informationen zu ergänzen
+fig.update_layout(
+    legend=dict(
+        title="Legende",
+        itemsizing="constant",
+        orientation="h",  # Horizontale Ausrichtung der Legende
+        x=0.5,  # Position der Legende
+        xanchor="center",
+        y=1.1,  # Position der Legende
+        yanchor="top",
+        bgcolor="LightSteelBlue",  # Hintergrundfarbe der Legende
+        bordercolor="Black",  # Rahmenfarbe der Legende
+        borderwidth=2,  # Rahmenbreite der Legende
+        font=dict(
+            family="Arial",
+            size=12,
+            color="Black"
         )
-    # Aktualisieren des Layouts, um die Legende hinzuzufügen
-    fig.update_layout(
-        legend=dict(
-            title="Legende",
-            itemsizing="constant"
     )
 )
     st.plotly_chart(fig)
