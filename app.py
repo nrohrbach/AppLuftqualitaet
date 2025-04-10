41c4e5e1f8ebe54c965eae8f9bd7351f7c24c62a
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2025-04-10 09:23:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,10 @@ if gemeinde:
     # Slider für das Jahr
     year = st.slider("Wählen Sie das Jahr", 1990, 2023, 2023)
     
-
+    # Initialisiere die Karte nur einmal
+    if 'm' not in st.session_state:
+        st.session_state['m'] = create_map(coordinatesOutput[2:4],year)
+        st.session_state['last_year'] = year
    
     # Zeige die Karte an
     #st_folium(st.session_state['m'], width=700)
